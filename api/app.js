65f8daf3db6f0aefa62284a165745eaf06579cb8
Granite Bagas <granitebagas28@gmail.com>
2023-12-10 02:57:28 +0700

    Setup cors

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const router = require('./src/router');
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static('public'));
 

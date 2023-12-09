3b65af02fb91413a8de9ec5d54438c0ee149ca50
Granite Bagas <granitebagas28@gmail.com>
2023-12-09 15:22:53 +0700

    Setup sequelize

---
+++ api/src/config/database.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: process.env.DB_DIALECT || 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+};

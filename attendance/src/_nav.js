3792523768afc93b382de550f77fd590d2f27666
Granite Bagas <granitebagas28@gmail.com>
2023-12-10 13:13:27 +0700

    Add attendance page

--- attendance/src/_nav.js
+++ attendance/src/_nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CIcon from '@coreui/icons-react';
-import { cilSpeedometer, cilUser } from '@coreui/icons';
+import { cilClock, cilSpeedometer, cilUser } from '@coreui/icons';
 import { CNavItem } from '@coreui/react';
 
 const _nav = [
@@ -15,6 +15,12 @@ const _nav = [
     name: 'Profile',
     to: '/profile',
     icon: <CIcon icon={cilUser} customClassName='nav-icon' />,
+  },
+  {
+    component: CNavItem,
+    name: 'Attendance',
+    to: '/attendance',
+    icon: <CIcon icon={cilClock} customClassName='nav-icon' />,
   },
 ];
 
--- attendance/src/routes.js
+++ attendance/src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Dashboard = React.lazy(() => import('./pages/Dashboard'));
 const Profile = React.lazy(() => import('./pages/Profile'));
+const Attendance = React.lazy(() => import('./pages/Attendance'));
 
 const routes = [
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/profile', name: 'Profile', element: Profile },
+  { path: '/attendance', name: 'Attendance', element: Attendance },
 ];
 
 export default routes;
